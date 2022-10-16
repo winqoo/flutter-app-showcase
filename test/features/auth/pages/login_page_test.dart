8b7b8f31996818da0f8f34254b5845dbf72111aa
Michal <winklermichu@gmail.com>
2022-10-16 21:21:29 +0200

    [test_task]add login Page navigation

--- test/features/auth/pages/login_page_test.dart
+++ test/features/auth/pages/login_page_test.dart
@@ -16,6 +16,7 @@ Future<void> main() async {
   late LoginPresenter presenter;
   late LoginNavigator navigator;
 
+  // ignore: no_leading_underscores_for_local_identifiers
   void _initMvp() {
     initParams = const LoginInitialParams();
     model = LoginPresentationModel.initial(
